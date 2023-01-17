19515cad3633d16e8512c1e3b5e742627e9bea31
sarah <68525964+sarahsidek@users.noreply.github.com>
2023-01-17 21:56:19 +0800

    animation for typing

---
+++ source/javascript.js
@@ -0,0 +1,23 @@
+
+// typing animation
+var typed = new Typed(".typing", {
+    strings:['', "Junior Frontend Developer"],
+    typeSpeed:100,
+    BackSpeed: 60,
+    loop:true
+})
+
+
+const nav = document.querySelector(".nav"),
+     navlist = nav.querySelectorAll("li"),
+     totalNavList = navlist.length;
+     for(let i=0; i<totalNavList; i++ ) {
+        const a = navlist[i].querySelector("a")
+        a.addEventListener("click", function(){
+            
+            for(let j=0; j<totalNavList; j++) {
+                navlist[j].querySelector("a").classList.remove("active")
+            }
+            this.classList.add("active")
+        })
+     }
